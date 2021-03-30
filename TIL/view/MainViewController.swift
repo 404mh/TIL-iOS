6e94589013a9415ccb9909eb2fac70085ee2480b
천수빈 <suebin710@gmail.com>
2021-03-31 08:52:42 +0900

    initial commit

---
+++ TIL/view/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  TIL
+//
+//  Created by exception on 2021/03/29.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var mypageButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "TIL"
+    }
+
+
+}
