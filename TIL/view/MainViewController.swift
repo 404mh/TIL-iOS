77e62b5f126a6fcd3eaa11b7c9bd8d3eedad0518
천수빈 <suebin710@gmail.com>
2021-04-04 18:37:13 +0900

    add firebase create function

--- TIL/view/MainViewController.swift
+++ TIL/view/MainViewController.swift
@@ -12,24 +12,25 @@ import FirebaseStorage
 class MainViewController: UIViewController {
     
     var ref: DatabaseReference!
-
+    var posts = [Post]()
+    var idx = 1
     // Navbar custom 필요
     @IBOutlet weak var mypageButton: UIBarButtonItem!
     @IBOutlet weak var inputField: UITextView!
     @IBOutlet weak var yilField: UILabel!
-    
+    @IBOutlet weak var writeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "TIL"
         inputField.backgroundColor = .systemPurple
-        
+        writeButton.setTitle("작성하기", for: .normal)
         yilField.text = "YIL"
     }
 
     @IBAction func writePost(_ sender: Any) {
         self.ref = Database.database().reference()
-
+            
         DispatchQueue.main.async(execute: {
             self.ref = Database.database().reference()
             
@@ -37,7 +38,10 @@ class MainViewController: UIViewController {
             formatter.dateFormat = "yyyy-MM-dd"
             let dateString = formatter.string(from: Date())
             
-            self.ref.child("Diary").child(dateString).setValue(self.inputField.text)
+            self.ref.child("Diary").child(dateString).child(String(self.idx)).setValue(self.inputField?.text)
+            
+            self.inputField.text = ""
+            self.idx += 1
         })
     }
     
