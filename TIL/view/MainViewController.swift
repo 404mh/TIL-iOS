ab613ab74db778afbd25b9f32f39665716115f81
천수빈 <suebin710@gmail.com>
2021-06-11 19:55:04 +0900

    add login button

--- TIL/view/MainViewController.swift
+++ TIL/view/MainViewController.swift
@@ -9,9 +9,7 @@ import UIKit
 import FirebaseDatabase
 import FirebaseStorage
 
-class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  {
-    
-    @IBOutlet weak var verticalCollectionView: UIView!
+class MainViewController: UIViewController {
     
     lazy var diary: [Post] = {
         var postList = [Post]()
@@ -22,7 +20,6 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
     // Navbar custom 필요
     @IBOutlet weak var mypageButton: UIBarButtonItem!
     @IBOutlet weak var inputField: UITextView!
-    @IBOutlet weak var yilField: UILabel!
     @IBOutlet weak var writeButton: UIButton!
     
     override func viewDidLoad() {
@@ -32,15 +29,7 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         self.title = "TIL"
         inputField.backgroundColor = .systemPurple
         writeButton.setTitle("작성하기", for: .normal)
-        yilField.text = "YIL"
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        verticalCollectionView.addSubview(collectionView)
-        collectionView.frame = CGRect(x: 0, y: 0, width: self.verticalCollectionView.frame.width, height: self.verticalCollectionView.frame.height)
-        
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         
     }
     
@@ -67,55 +56,9 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.diary.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .cyan
-        for subView in cell.subviews {
-            subView.removeFromSuperview()
-        }
-        
-        let label: UILabel = {
-            let lblTxt = UILabel()
-            lblTxt.text = "\(indexPath.row)"
-            lblTxt.textAlignment = .center
-            return lblTxt
-        }()
-        
-        cell.addSubview(label)
-        label.frame = CGRect(x: 0, y: 0, width: cell.frame.width, height: cell.frame.height)
-        
-        return cell
-    }
-    
-    
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 0
-        
-        layout.scrollDirection = .vertical
-        layout.sectionInset = .zero
-        
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .systemGreen
-        
-        return cv
-    }()
+  
     
     func updateData() {
-        ref.child("Diary").child("user").child("date").observe(.value, with: {(snapshot) in
-            
-            let values = snapshot.value as? [String: AnyObject] ?? [:]
-            for element in values {
-            }
-            DispatchQueue.main.async {
-                self.collectionView.reloadData()
-                
-            }
-            
-        })
+        
     }
 }
