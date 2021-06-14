93ffe9970ea9032b5fe6be61f6ae59d8fcb93085
천수빈 <suebin710@gmail.com>
2021-06-14 13:28:28 +0900

    update podfile

--- TIL/view/LoginViewController.swift
+++ TIL/view/LoginViewController.swift
@@ -11,27 +11,29 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var appleLoginButton: UIImageView!
-    @IBOutlet weak var githubLoginButton: UIImageView!
+    @IBOutlet weak var githubLoginButton: UIButton!
     
     var provider = OAuthProvider(providerID: "github.com")
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func githubLogin(_ sender: Any) {
         
     }
     
-
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
